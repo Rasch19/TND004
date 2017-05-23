9c453de3b329f0691d6776e376ac255a8eeaec73
Rasch19 <johannes.s.o.rasch@gmail.com>
2017-05-23 11:01:08 +0200

    PartB
    fixed part B

--- Labb4/Code4b/graph.cpp
+++ Labb4/Code4b/graph.cpp
@@ -8,6 +8,7 @@
 #include <iostream>
 #include <iomanip>
 #include <cassert>
+#include <vector>
 
 using namespace std;
 
@@ -58,13 +59,114 @@ void Graph::removeEdge(int u, int v)
 // Prim's minimum spanning tree algorithm
 void Graph::mstPrim() const
 {
-    // *** TODO ***
+	vector<Edge> edge;
+
+	//allocate memory for values needed
+	//can do without any of these..?
+	int *dist = new int[size + 1];
+	int *path = new int[size + 1];
+	bool *done = new bool[size + 1];
+
+	// init values of the list
+	for (int i = 1; i <= size; i++)
+	{
+		dist[i] = INF;
+		path[i] = 0;
+		done[i] = false;
+	}
+
+	int s = 1, totalWeight = 0;; // s = start position
+	dist[s] = 0;
+	done[s] = true;
+
+	// set shortest paths for the graph
+	while (true)
+	{
+		Node *adjacent = array[s].getFirst();
+
+		while (adjacent)
+		{
+			if (!done[adjacent->vertex] && dist[adjacent->vertex] > adjacent->weight)
+			{
+				dist[adjacent->vertex] = adjacent->weight;              //the distance from the origin
+				path[adjacent->vertex] = s;                             //the path goes from v
+			}
+			adjacent = array[s].getNext(); //check next adjacent vector
+		}
+
+		done[s] = true;
+
+		edge.push_back(Edge(s, path[s], dist[s]));
+
+		int smallest = INF;
+
+		// look for smallest distance vertex
+		for (unsigned int i = 1; i <= size; i++)
+		{
+			if (!done[i] && dist[i] < smallest)
+			{
+				smallest = dist[i];
+				s = i;
+			}
+		}
+
+		if (smallest == INF) break;
+	}
+
+	// calculate total weight
+	for (unsigned int i = 1; i < edge.size(); ++i)
+	{
+		std::cout << "( " << edge[i].tail << ", " << edge[i].head << ". " << edge[i].weight << ")" << endl;
+		totalWeight += edge[i].weight;
+	}
+
+	std::cout << "Total weight: " << totalWeight << endl
+		<< "=====================================================================" << endl;
+
+	//deallocate memory
+	delete[] dist;
+	delete[] path;
+	delete[] done;
 }
 
 // Kruskal's minimum spanning tree algorithm
 void Graph::mstKruskal() const
 {
-    // *** TODO ***
+	Heap<Edge> H;
+	DSets D(size);
+
+	int total = 0, counter = 0;
+
+	//H.heapify(H);
+	for (int i = 1; i <= size; i++)
+	{
+		Node *current = array[i].getFirst();
+		while (current)
+		{
+			if (current->vertex < i)
+			{
+				H.insert(Edge(current->vertex, i, current->weight));
+			}
+			current = array[i].getNext();
+		}
+	}
+	//Heapification done
+
+	//find new edges
+	while (counter < size - 1) //number of edges must be n-1
+	{
+		Edge n = H.deleteMin();
+		if (D.find(n.head) != D.find(n.tail))
+		{
+			D.join(D.find(n.head), D.find(n.tail));
+			counter++;
+			total += n.weight;
+			cout << n << endl;
+		}
+
+	}
+	cout << endl << "Total weight: " << total << endl
+		<< "=====================================================================" << endl;
 }
 
 // print graph
